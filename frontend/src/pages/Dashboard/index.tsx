925632b6227bf5efc77ac72549c49e1c36f57c0c
Leonardo correa <leonardocorreac@gmail.com>
2021-11-07 19:24:57 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,33 +1,37 @@
 import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
+import DataTable from "components/DataTable/index";
 import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
-const Dashboard = () => {
+function Dashboard(): JSX.Element {
     return (
         <>
             <NavBar />
             <div className="container">
-                <h1 className="text-primary py-6"> Dashboard de Vendas</h1>
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
                 <div className="row px-3">
                     <div className="col-sm-6">
-                        <h5 className="text center text-secondary">Taxa de Sucesso(%)</h5>
+                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
-                        <h5 className="text center text-secondary">Todas Vendas</h5>
+                        <h5 className="text-center text-secondary">Todas Vendas</h5>
                         <DonutChart />
                     </div>
-                    <div className="py3">
-                        <h2 className="text-primary">Todas Vendas </h2>
-                    </div>
-                    <DataTable />
                 </div>
-                <Footer />
+
+                <div className="py-3">
+                    <h2 className="text-primary"> Todas Vendas</h2>
+                </div>
+
+                <DataTable />
             </div>
+            <Footer />
         </>
     );
+
 }
 
 export default Dashboard;
